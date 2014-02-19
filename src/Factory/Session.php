60d435ead8138b036630c064f97ab968585abb70
Joel Wurtz <jwurtz@jolicode.com>
2014-02-19 01:33:31 +0100

    Add session factory

---
+++ src/Factory/Session.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pyrite\Factory;
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Stack\Session as StackSession;
+
+class Session implements HttpKernelFactory
+{
+    public function register(HttpKernelInterface $app = null, $name = '', array $parameters = array())
+    {
+        if (null === $app) {
+            throw new \RuntimeException("Session must have a wrapped kernel");
+        }
+        
+        return array($name, new StackSession($app, $parameters));
+    }
+}
