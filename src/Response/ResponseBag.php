f04aecf5b316d35717d075a62f223fc862a91b2f
remievaneos <remi@evaneos.com>
2014-04-10 17:26:51 +0200

    added the missing constant used by the viewrendererLayer

--- src/Response/ResponseBag.php
+++ src/Response/ResponseBag.php
@@ -5,6 +5,8 @@ namespace Pyrite\Response;
 
 interface ResponseBag
 {
+    const ACTION_RESULT = 'ACTION_RESULT';
+    
     function set($key, $value);
     function get($key, $defaultValue = null);
     function has($key);
