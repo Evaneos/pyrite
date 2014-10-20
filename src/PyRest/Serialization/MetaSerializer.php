c0afde2cc3268caf3a11cf8325dfd7eb94e0790b
Olivier Madre <terenas@gmail.com>
2014-10-20 18:42:52 +0200

    fixed meta when primitive

--- src/PyRest/Serialization/MetaSerializer.php
+++ src/PyRest/Serialization/MetaSerializer.php
@@ -3,6 +3,7 @@
 namespace Pyrite\PyRest\Serialization;
 
 use Pyrite\PyRest\PyRestObject;
+use Pyrite\PyRest\PyRestObjectPrimitive;
 use Pyrite\PyRest\PyRestUrlGenerator;
 use Pyrite\PyRest\Type\PyRestItem;
 use Pyrite\PyRest\Type\PyRestProperty;
@@ -28,39 +29,43 @@ class MetaSerializer implements Serializer
 
     public function serializeOne(PyRestObject $object, array $options = array())
     {
-        $out['resource'] = $object::RESOURCE_NAME;
-        $out['links'] = array();
-        $out['links']['self'] = $this->urlGenerator->getItem($object::RESOURCE_NAME, $object->getId());
+        $out = array();
+        if (!($object instanceof PyRestObjectPrimitive)) {
+            $out['resource'] = $object::RESOURCE_NAME;
+            $out['links'] = array();
 
-        $out['embeds'] = array();
+            $out['links']['self'] = $this->urlGenerator->getItem($object::RESOURCE_NAME, $object->getId());
 
-        foreach($object->getEmbeddables() as $embeddableName => $embedDefinition) {
-            $out['embeds'][$embeddableName] = array();
+            $out['embeds'] = array();
 
-            if (!($embedDefinition instanceof PyRestProperty)) {
-                $out['embeds'][$embeddableName]['resource'] = $embedDefinition->getResourceType();
-            }
+            foreach($object->getEmbeddables() as $embeddableName => $embedDefinition) {
+                $out['embeds'][$embeddableName] = array();
 
-            if ($embedDefinition instanceof PyRestItem) {
-                $out['embeds'][$embeddableName]['type'] = 'item';
-            }
-            elseif ($embedDefinition instanceof PyRestProperty) {
-                $out['embeds'][$embeddableName]['type'] = 'property';
-            }
-            else {
-                $out['embeds'][$embeddableName]['type'] = 'collection';
-            }
+                if (!($embedDefinition instanceof PyRestProperty)) {
+                    $out['embeds'][$embeddableName]['resource'] = $embedDefinition->getResourceType();
+                }
 
-            $out['embeds'][$embeddableName]['links'] = array();
+                if ($embedDefinition instanceof PyRestItem) {
+                    $out['embeds'][$embeddableName]['type'] = 'item';
+                }
+                elseif ($embedDefinition instanceof PyRestProperty) {
+                    $out['embeds'][$embeddableName]['type'] = 'property';
+                }
+                else {
+                    $out['embeds'][$embeddableName]['type'] = 'collection';
+                }
 
-            if (!($embedDefinition instanceof PyRestProperty)) {
-                $out['embeds'][$embeddableName]['links']['collection'] = $this->urlGenerator->getCollection($embedDefinition->getResourceType());
-                $out['embeds'][$embeddableName]['links']['nested'] = $this->urlGenerator->getNested($object::RESOURCE_NAME, $object->getId(), $embeddableName);
-            }
+                $out['embeds'][$embeddableName]['links'] = array();
+
+                if (!($embedDefinition instanceof PyRestProperty)) {
+                    $out['embeds'][$embeddableName]['links']['collection'] = $this->urlGenerator->getCollection($embedDefinition->getResourceType());
+                    $out['embeds'][$embeddableName]['links']['nested'] = $this->urlGenerator->getNested($object::RESOURCE_NAME, $object->getId(), $embeddableName);
+                }
 
 
-            if (($embedDefinition instanceof PyRestItem) || ($embedDefinition instanceof PyRestProperty)) {
-                $out['embeds'][$embeddableName]['links']['embed'] = $this->urlGenerator->getItem($object::RESOURCE_NAME, $object->getId()) . '?embed=' . $embeddableName;
+                if (($embedDefinition instanceof PyRestItem) || ($embedDefinition instanceof PyRestProperty)) {
+                    $out['embeds'][$embeddableName]['links']['embed'] = $this->urlGenerator->getItem($object::RESOURCE_NAME, $object->getId()) . '?embed=' . $embeddableName;
+                }
             }
         }
 
