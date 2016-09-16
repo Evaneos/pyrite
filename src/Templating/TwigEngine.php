74936f2ba46d98a0130dc71efb838d018f4e8fc6
Alexandre Dupuy <alex.dpy@gmail.com>
2016-09-16 12:13:07 +0200

    twig: add DumpExtension if VarDumper and TwigBridge are available

--- src/Templating/TwigEngine.php
+++ src/Templating/TwigEngine.php
@@ -2,8 +2,10 @@
 
 namespace Pyrite\Templating;
 
+use Symfony\Bridge\Twig\Extension\DumpExtension;
 use Pyrite\Container\Container;
 use Pyrite\Templating\Twig\Extension;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
 
 class TwigEngine implements Engine
 {
@@ -13,12 +15,17 @@ class TwigEngine implements Engine
     {
         $rootDir        = $container->getParameter('root_dir');
         $productionMode = $container->getParameter('production_mode');
+        $debug = !($productionMode === true);
 
         $loader = new \Twig_Loader_Filesystem($rootDir);
         $this->twig = new \Twig_Environment($loader, array(
             'cache' => $rootDir . '/tmp',
-            'debug' => !($productionMode === true)
+            'debug' => $debug
         ));
+
+        if ($debug && class_exists('Symfony\Component\VarDumper\Cloner\VarCloner') && class_exists('Symfony\Bridge\Twig\Extension\DumpExtension')) {
+            $this->twig->addExtension(new DumpExtension(new VarCloner()));
+        }
     }
 
     public function extendTwig(Extension $extension)
