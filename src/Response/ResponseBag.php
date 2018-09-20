922126bd86807015b224d1c711b89dcab3ddef34
Yoann Blot <yoann.blot@gmail.com>
2018-09-20 16:55:48 +0200

    test(pyrite): Response Bag

--- src/Response/ResponseBag.php
+++ src/Response/ResponseBag.php
@@ -6,27 +6,92 @@ interface ResponseBag
 {
     const ACTION_RESULT = 'ACTION_RESULT';
 
-    const TYPE_DEFAULT = 'DEFAULT';
+    const TYPE_DEFAULT  = 'DEFAULT';
     const TYPE_STREAMED = 'STREAMED';
-    const TYPE_BINARY = 'BINARY';
+    const TYPE_BINARY   = 'BINARY';
 
+    /**
+     * @param string $key
+     * @param mixed  $value
+     *
+     * @return self
+     */
     public function set($key, $value);
+
+    /**
+     * @param string $key
+     * @param mixed  $defaultValue
+     *
+     * @return mixed
+     */
     public function get($key, $defaultValue = null);
+
+    /**
+     * @return array
+     */
     public function getAll();
+
+    /**
+     * @param string $key
+     *
+     * @return bool
+     */
     public function has($key);
 
+    /**
+     * @param string $value
+     *
+     * @return self
+     */
     public function setResult($value);
+
+    /**
+     * @return string
+     */
     public function getResult();
 
+    /**
+     * @param int $value
+     *
+     * @return self
+     */
     public function setResultCode($value);
+
+    /**
+     * @return int
+     */
     public function getResultCode();
 
+    /**
+     * @param string $key
+     * @param string $value
+     */
     public function addHeader($key, $value);
+
+    /**
+     * @return string[]
+     */
     public function getHeaders();
 
+    /**
+     * @param callable $callback
+     */
     public function setCallback($callback);
+
+    /**
+     * @return callable
+     */
     public function getCallback();
 
+    /**
+     * @param string $type
+     *
+     * @throws \InvalidArgumentException
+     */
     public function setType($type);
+
+    /**
+     * @return string
+     */
     public function getType();
 }
--- src/Response/ResponseBagImpl.php
+++ src/Response/ResponseBagImpl.php
@@ -4,36 +4,43 @@ namespace Pyrite\Response;
 
 class ResponseBagImpl implements ResponseBag
 {
-    /** @var array  */
-    protected $data = array();
+    /** @var array */
+    protected $data = [];
 
-    /** @var array  */
-    protected $errors = array();
+    /** @var array */
+    protected $errors = [];
 
-    /** @var string  */
+    /** @var string */
     protected $result = '';
 
-    /** @var int  */
+    /** @var int */
     protected $resultCode = 200;
 
-    /** @var array  */
-    protected $headers = array();
+    /** @var string[] */
+    protected $headers = [];
 
-    /** @var string  */
+    /** @var string */
     protected $type = self::TYPE_DEFAULT;
 
-    /** @var  Callable */
+    /** @var callable */
     protected $callback;
 
+    /**
+     * @inheritdoc
+     */
     public function set($key, $value)
     {
         $this->data[$key] = $value;
+
         return $this;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function setType($type)
     {
-        if(!in_array($type, array(self::TYPE_STREAMED, self::TYPE_BINARY, self::TYPE_DEFAULT))){
+        if (!in_array($type, [self::TYPE_STREAMED, self::TYPE_BINARY, self::TYPE_DEFAULT], true)) {
             throw new \InvalidArgumentException('Unknown response type');
         }
 
@@ -41,66 +48,105 @@ class ResponseBagImpl implements ResponseBag
         $this->type = $type;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getType()
     {
         return $this->type;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function get($key, $defaultValue = null)
     {
         if (array_key_exists($key, $this->data)) {
             return $this->data[$key];
         }
+
         return $defaultValue;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getAll()
     {
         return $this->data;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function has($key)
     {
         return array_key_exists($key, $this->data);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function setResult($value)
     {
         $this->result = $value;
+
         return $this;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getResult()
     {
         return $this->result;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function setCallback($callback)
     {
         $this->callback = $callback;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getCallback()
     {
         return $this->callback;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function setResultCode($value)
     {
         $this->resultCode = $value;
+
         return $this;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getResultCode()
     {
         return $this->resultCode;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function addHeader($key, $value)
     {
         $this->headers[$key] = $value;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getHeaders()
     {
         return $this->headers;
